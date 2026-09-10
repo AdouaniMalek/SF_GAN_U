967d8a14b2b7ee97ff99d589815a16da1db11dd2
AdouaniMalek <66918381+AdouaniMalek@users.noreply.github.com>
2026-09-10 10:55:22 +0200

    Update dataset directory path in Architecture.py

--- Dataset_1/Architecture.py
+++ Dataset_1/Architecture.py
@@ -44,7 +44,7 @@ warnings.filterwarnings("ignore")
 # =============================================================================
 
 # --- Paths ---
-DATASET_DIR = "C:/Users/Malek Adouani/Desktop/SF_GAN_U/Dataset_1/"
+DATASET_DIR = "C:/Users/Malek Adouani/Desktop/SF_GAN/Dataset_1/"
 MODEL_DIR   = os.path.join(DATASET_DIR, "models")
 os.makedirs(MODEL_DIR, exist_ok=True)
 
